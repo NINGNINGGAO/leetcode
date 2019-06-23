f492cbd4213ada80b329fa3eb00772299d25b0c3
NINGNINGGAO <707288044@qq.com>
2019-06-23 17:58:27 +0800

    Update 6/23 17/58

--- c/c_5_1.c
+++ c/c_5_1.c
@@ -1,57 +1,31 @@
-char g_subStr[1002] = {0};
-#define MIN(a, b) ((a < b) ? a : b)
+#define max(a,b) (((a) > (b)) ? (a) : (b))
 
-char *longestPalindrome(char *s) 
-{
-	int i = 0;
-	int j = 0;
-	int len;
-	int max = 0;
-	int center = 0;
-	int rPos = 0;
-	int r_center = 0;
-	int p[2003] = {0};
-	char extendStr[2003] = {0};
-
-	if (s == NULL) {
-		return NULL;
-	}
-
-	len = strlen(s);
-	if (len == 0 || len == 1) {
-		return s;
-	}
-
-	extendStr[i++] = '(';
-	while (i < (len * 2 + 1)) {
-		extendStr[i++] = '#';
-		extendStr[i++] = s[j++];
-	}
-	extendStr[i++] = '#';
-	extendStr[i] = 0;
-
-	for (i = 1; i < len * 2 + 2; i++) {
-		p[i] = (i<rPos) ? (MIN(rPos - i, p[r_center * 2 - i])) : 1;
-		
-		while (extendStr[i - p[i]] == extendStr[i + p[i]]) {
-			p[i]++;
-		}
-		
-		if (i + p[i] > rPos) {
-			rPos = i + p[i];
-			r_center = i;
-		}
-
-		if (p[i] > max) {
-			max = p[i];
-			center = i;
-		}
-	}
-
-	j = (center - max + 1) / 2;
-	for (i = 0; i < max - 1; i++) {
-		g_subStr[i] = s[j++];
-	}
-	g_subStr[i] = 0;
-	return g_subStr;
+char * longestPalindrome(char * s){
+    if (strlen(s) == 0) return "";
+    int start = 0, end = 0;
+    for (int i = 0; i < strlen(s); i++) {
+        int len1 = expandaroundcenter(s, i, i);
+        int len2 = expandaroundcenter(s, i, i+1);
+        int len = max(len1, len2);
+        if (len > end - start) {
+            start = i - ((len - 1)>>1);
+            end = i + (len>>1);
+        }
+    }
+    char *p = malloc(sizeof(char) * (end - start + 2));
+    //char p[end - start +2];
+    strncpy(p, s + start, end - start + 1);
+    p[end - start + 1] = '\0';
+    return p;
 }
+
+int expandaroundcenter(char *s, int left, int right)
+{
+    int L = left, R = right;
+    while (L >= 0 && R < strlen(s) && s[L] == s[R]) {
+        L--;
+        R++;
+    }
+    return R - L - 1;
+}
+
