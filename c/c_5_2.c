0f3c720cb09ceba403038e20338b43b65dc5ed61
NINGNINGGAO <707288044@qq.com>
2019-06-23 21:05:48 +0800

    Update 6/23 21/05

--- c/c_5_2.c
+++ c/c_5_2.c
@@ -28,8 +28,9 @@ char *longestPalindrome(char *s)
             RL[i] = 1;
         }
         
-        while (i - RL[i] >= 0 && i + RL[i] < len2 && s1[i - RL[i]] == s1[i + RL[i]])
+        while (i - RL[i] >= 0 && i + RL[i] < len2 && s1[i - RL[i]] == s1[i + RL[i]]) {
             RL[i] += 1;
+	}
         
         if (RL[i] + i - 1 > MaxRight) {
             MaxRight = RL[i] + i - 1;
