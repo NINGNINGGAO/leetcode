9780519cb58308ebf5de9c4deb5b406d088b8820
NINGNINGGAO <707288044@qq.com>
2019-06-26 09:55:34 +0800

    6/26 09/55

---
+++ c/c_9_1.c
@@ -0,0 +1,17 @@
+bool isPalindrome(int x){
+    if (x < 0 || (x % 10 == 0 && x != 0)) return false; 
+    int res = 0;
+    while (x > res) {
+        res = res * 10 + x % 10;
+        x = x / 10;
+    }
+    
+    if (x < res){
+        res /= 10;
+        if (x != res) {
+            return false;
+        }
+    }
+    return true;
+}
+
