f04bb7847935237a5fc22ae747bbd16cbab2b69c
NINGNINGGAO <agogin@qq.com>
2019-08-18 16:53:29 +0800

    add c_29_1.c

---
+++ c/c_29_1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <limits.h>
+
+
+
+int divide(int dividend, int divisor){
+    if (dividend == 0) return 0;
+    int flag = (dividend ^ divisor) >= 0 ? 0 : 1;
+    if (divisor > 0) divisor = -divisor;
+    if (dividend > 0) dividend = -dividend;
+    if (dividend > divisor) return 0;
+    int ret = 0;
+    int temp, mid;
+    while (dividend != 0) {
+        temp = divisor;
+        mid = 1;
+        if (dividend > divisor) break;
+        while (dividend - temp < temp) {
+            temp = -temp;
+            temp <<= 1;
+            mid <<= 1;
+            temp = -temp;
+        }
+        ret += -mid;
+        dividend -= temp;
+    }
+    if (flag == 0) {
+        if (ret == INT_MIN) return INT_MAX;
+        return -ret;
+    }
+    return ret;
+}
+
+main() {
+    int dividend = 1;
+    int divisor = 1;
+    printf("%d\n", divide(dividend, divisor));
+}
