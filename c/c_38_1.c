8923c7a48d834db43ca135a2d37b0b6469f4ecca
NINGNINGGAO <agogin@qq.com>
2019-08-20 14:44:04 +0800

    add c_38_1.c 8/20 14/44

---
+++ c/c_38_1.c
@@ -0,0 +1,41 @@
+/*
+ * @Author: AGOGIN 
+ * @Date: 2019-08-20 13:11:50 
+ * @Last Modified by: AGOGIN
+ * @Last Modified time: 2019-08-20 13:37:15
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+char * countAndSay(int n){
+    char *ret = malloc(sizeof(char) * 500);
+    char temp[500];
+    ret[0] = '1';
+    ret[1] = '\0';
+    int count = 1;
+    int mid = 0, j = 0;
+    for (int i = 1; i < n; i++) {
+        while (ret[j] != '\0') {
+            if (ret[j] == ret[j + 1]) {
+                count++;
+            } else {
+                temp[mid++] = count + '0';
+                temp[mid++] = ret[j];
+                count = 1;
+            }
+            j++;
+        }
+        temp[mid] = '\0';
+        ret = strcpy(ret, temp);
+        mid = 0;
+        j = 0;
+        count = 1;
+    }
+    return ret;
+}
+
+main() {
+    char *p = malloc(sizeof(char) * 500);
+    p = countAndSay(25);
+    printf("%s\n", p);
+}
