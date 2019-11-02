eaf61927487c6eb0fd0bca919fcd72f69ec4e1ea
NINGNINGGAO <agogin@qq.com>
2019-11-02 20:27:47 +0800

    add c_278_1 19/11/02 20/27

--- c/c_278_1.c
+++ c/c_278_1.c
@@ -2,7 +2,7 @@
  * @Author: AGOGIN
  * @Date: 2019-11-02 11:19:44
  * @Last Modified by: AGOGIN
- * @Last Modified time: 2019-11-02 12:10:41
+ * @Last Modified time: 2019-11-02 20:27:07
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,9 +15,11 @@ bool isBadVersion(int version);
 int firstBadVersion(int n) {
     int p = 1, q = n;
     int mid = p;
+    if (isBadVersion(p) == true) return mid;
     while (p < q) {
+        if (q - p == 1) return q;
         mid = p + (q - p) / 2;
-        if (isBadVersion(mid) == true) p = mid;
+        if (isBadVersion(mid) == false) p = mid;
         else q = mid;
     }
     return mid;
