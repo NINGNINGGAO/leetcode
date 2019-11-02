3da17e49f60eb6d7c0f48e807964fc6bfda469d9
NINGNINGGAO <agogin@qq.com>
2019-11-02 21:53:20 +0800

    add c_326_1.c 19/11/02 21/53

---
+++ c/c_326_1.c
@@ -0,0 +1,19 @@
+/*
+ * @Author: AGOGIN
+ * @Date: 2019-11-02 21:52:49
+ * @Last Modified by:   AGOGIN
+ * @Last Modified time: 2019-11-02 21:52:49
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <stdbool.h>
+bool isPowerOfThree(int n){
+    return n > 0 && 1162261467 % n == 0;
+}
+
+int main() {
+
+    return 0;
+}
