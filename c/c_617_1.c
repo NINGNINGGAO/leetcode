a941c21c80c95b044e3f7099fffd8dfd3a554d36
NINGNINGGAO <agogin@qq.com>
2019-11-16 11:38:13 +0800

    modify c_617_1 19/11/16 11/38

--- c/c_617_1.c
+++ c/c_617_1.c
@@ -2,7 +2,7 @@
  * @Author: AGOGIN
  * @Date: 2019-11-16 11:25:08
  * @Last Modified by: AGOGIN
- * @Last Modified time: 2019-11-16 11:34:52
+ * @Last Modified time: 2019-11-16 11:37:41
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,14 +18,8 @@ struct TreeNode* mergeTrees(struct TreeNode* t1, struct TreeNode* t2){
     if (t1 == NULL) return t2;
     if (t2 == NULL) return t1;
     t1->val += t2->val;
-    if (t1->left == NULL) t1->left = t2->left;
-    else {
-        t1->left = mergeTrees(t1->left, t2->left);
-    }
-    if (t1->right == NULL) t1->right = t2->right;
-    else {
-        t1->right = mergeTrees(t1->right, t2->right);
-    }
+    t1->left = mergeTrees(t1->left, t2->left);
+    t1->right = mergeTrees(t1->right, t2->right);
     return t1;
 }
 
