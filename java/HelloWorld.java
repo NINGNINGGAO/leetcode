495066ae5c166133bccc814766941536244434f7
NINGNINGGAO <agogin@qq.com>
2020-04-14 22:52:49 +0800

    chang location

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.nextLine());
     }
 }
